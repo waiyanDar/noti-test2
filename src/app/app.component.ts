db5d38ddfc2e544fbbc0452bf5438dd2059a6462
waiyanmyintmyat <waiyanmyintmyat@startinpoint.com>
2024-12-05 15:41:49 +0630

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { getToken, Messaging, onMessage } from '@angular/fire/messaging';
 import { RouterOutlet } from '@angular/router';
+import { EMPTY, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,39 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'noti-test-by-wy';
+  message$: Observable<any> = EMPTY;
+
+  private messaging = inject(Messaging);
+
+  constructor() {
+    console.log("messaging : ", this.messaging);
+    Notification.requestPermission().then(
+      (notificationPermissions: NotificationPermission) => {
+        if (notificationPermissions === "granted") {
+          console.log("Granted");
+        }
+        if (notificationPermissions === "denied") {
+          console.log("Denied");
+        }
+      });
+    navigator.serviceWorker
+      .register("../assets/firebase-messaging-sw.js", {
+        type: "module",
+      })
+      .then((serviceWorkerRegistration) => {
+        getToken(this.messaging, {
+          vapidKey: `BLrdwVhyMfINgeXI5tge1fOiGI41DUjFSfWiwvLVDNrFD73qeyH5RVE4tlf0Ov6z8_15i8tvxSKNZfu10pQBPJM`,
+          serviceWorkerRegistration: serviceWorkerRegistration,
+        }).then((x) => {
+          console.log('my fcm token', x);
+          // This is a good place to then store it on your database for each user
+        });
+      });
+    this.message$ = new Observable((sub) => onMessage(this.messaging, (msg) =>
+      sub.next(msg))).pipe(
+        tap((msg) => {
+          console.log("My Firebase Cloud Message", msg);
+        })
+      );
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp({
+      "projectId":"noti-test-by-wy",
+      "appId":"1:96202926342:web:abcc4cdebc53a2cc5e9687",
+      "storageBucket":"noti-test-by-wy.firebasestorage.app",
+      "apiKey":"AIzaSyB5ydyjo9O9blpgSdRynO1ljSYcVi0vYgs",
+      "authDomain":"noti-test-by-wy.firebaseapp.com",
+      "messagingSenderId":"96202926342",
+      "measurementId":"G-BR6YE7E9ZG"
+    })), 
+    provideMessaging(() => getMessaging()), 
+    provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 };
