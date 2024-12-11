096c2fa6688dd5ce33532845f96e3a0e0d7b7b38
waiyanmyintmyat <waiyanmyintmyat@startinpoint.com>
2024-12-11 13:18:07 +0630

    remove redunant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { getToken, Messaging, onMessage } from '@angular/fire/messaging';
 import { RouterOutlet } from '@angular/router';
-import { EMPTY, Observable, tap } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 import { FcmService } from './service/fcm.service';
 
 @Component({
@@ -17,39 +16,7 @@ export class AppComponent implements OnInit{
 
   private fcmService = inject (FcmService);
 
-  // private messaging = inject(Messaging);
-
-  constructor() {
-    // console.log("messaging : ", this.messaging);
-    // Notification.requestPermission().then(
-    //   (notificationPermissions: NotificationPermission) => {
-    //     if (notificationPermissions === "granted") {
-    //       console.log("Granted");
-    //     }
-    //     if (notificationPermissions === "denied") {
-    //       console.log("Denied");
-    //     }
-    //   });
-    // navigator.serviceWorker
-    //   .register("../assets/firebase-messaging-sw.js", {
-    //     type: "module",
-    //   })
-    //   .then((serviceWorkerRegistration) => {
-    //     getToken(this.messaging, {
-    //       vapidKey: `BLrdwVhyMfINgeXI5tge1fOiGI41DUjFSfWiwvLVDNrFD73qeyH5RVE4tlf0Ov6z8_15i8tvxSKNZfu10pQBPJM`,
-    //       serviceWorkerRegistration: serviceWorkerRegistration,
-    //     }).then((x) => {
-    //       console.log('my fcm token', x);
-    //       // This is a good place to then store it on your database for each user
-    //     });
-    //   });
-    // this.message$ = new Observable((sub) => onMessage(this.messaging, (msg) =>
-    //   sub.next(msg))).pipe(
-    //     tap((msg) => {
-    //       console.log("My Firebase Cloud Message", msg);
-    //     })
-    //   );
-  }
+  constructor() {}
 
   ngOnInit(): void {
     Notification.requestPermission().then(
--- src/app/service/fcm.service.ts
+++ src/app/service/fcm.service.ts
@@ -3,38 +3,29 @@ import { getToken, Messaging, onMessage } from "@angular/fire/messaging";
 import { EMPTY, Observable, tap } from "rxjs";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class FcmService {
 
-    private messaging = inject(Messaging);
-    message$: Observable<any> = EMPTY;
-    constructor() { }
+  private messaging = inject(Messaging);
+  message$: Observable<any> = EMPTY;
+  constructor() { }
 
-    reuquestPermission() {
-        getToken(this.messaging, {
-            vapidKey: 'BLrdwVhyMfINgeXI5tge1fOiGI41DUjFSfWiwvLVDNrFD73qeyH5RVE4tlf0Ov6z8_15i8tvxSKNZfu10pQBPJM'
-        })
-            .then(token => {
-                console.log("My Firebase Cloud Message token : ", token);
-            })
-            .catch(err => {
-                console.log("My Firebase Cloud Message error : ", err);
-            })
-    }
+  reuquestPermission() {
+    getToken(this.messaging, {
+      vapidKey: 'BLrdwVhyMfINgeXI5tge1fOiGI41DUjFSfWiwvLVDNrFD73qeyH5RVE4tlf0Ov6z8_15i8tvxSKNZfu10pQBPJM'
+    })
+      .then(token => {
+        console.log("My Firebase Cloud Message token : ", token);
+      })
+      .catch(err => {
+        console.log("My Firebase Cloud Message error : ", err);
+      })
+  }
 
-    listenMessages() {
-        console.log('Listening for messages...');
-    
-        this.message$ = new Observable((subscriber) => {
-          onMessage(this.messaging, (payload) => {
-            console.log('Foreground message received:', payload);
-            subscriber.next(payload); // Pass the message to subscribers
-          });
-        });
-    
-        this.message$.subscribe({
-          next: (message) => console.log('Processed foreground message:', message),
-        });
-      }
+  listenMessages() {
+    onMessage(this.messaging, (payload) => {
+      console.log("My Firebase Cloud Message payload : ", payload);
+    })
+  }
 }
